a4ada25c7016f033e88f0ed91987d957fa31d71e
peterdotw <piotrwiderapraca@gmail.com>
2020-05-20 00:56:06 +0200

    Song model done

---
+++ Models/Song.cs
@@ -0,0 +1,10 @@
+namespace SongCollector.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+    }
+}
